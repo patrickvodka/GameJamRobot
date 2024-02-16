bf9b946e4f7e339272988ca7c7de934183d8a05e
Shiva <wizi97131@gmail.com>
2024-02-16 13:57:48 +0100

    finish p

--- Assets/Script/FinishPoint.cs
+++ Assets/Script/FinishPoint.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishPoint : MonoBehaviour
 {
@@ -9,7 +10,7 @@ public class FinishPoint : MonoBehaviour
     {
         if (collider.CompareTag("Player"))
         {
-            var gameState = GameManager.GameState.Victory;
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 
