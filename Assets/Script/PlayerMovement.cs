638c321edd14ce2f794d171dd01bb8c4e0ae970b
Patrick <LeNotraldu22@gmail.com>
2024-02-16 04:20:23 +0100

    PlayerMovement Rota

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -2,31 +2,31 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
-using Unity.Mathematics;
 using UnityEngine;
-
+using UnityEngine.UIElements;
 [RequireComponent(typeof(GhostTrail))]
+
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
     public float playerSpeed;
     public float jumpForce;
-    private Vector3 velocity = Vector2.zero;
+    private Vector3 velocity= Vector2.zero;
     private float horisontalMovemant;
     [SerializeField] private LayerMask ground;
     [SerializeField] private float CooldownJump = .5f;
-    private float TimeSinceJump = 6.0f;
-    [Header("SpawnOfPlayer")] 
+    private float TimeSinceJump = .2f;
+    [Header("SpawnOfPlayer")]
     private bool firstInput;
-    [HideInInspector] public bool thing;
-    private bool blockInput;
+    [HideInInspector]public bool thing;
+    private bool isRebooting;
     private GhostTrail ghostTrail;
     [SerializeField] [NotNull] public Transform groundCheck;
-    private const string HorizontalAxis = "Horizontal"; 
-    private const float InputThreshold = 0.1f; 
-    private const float RotationZero = 0f; 
+    private const string HorizontalAxis = "Horizontal";
+    private const float InputThreshold = 0.1f;
+    private const float RotationZero = 0f;
+    private bool blockInput;
     public ParticleSystem explosion;
-
     void Start()
     {
         firstInput = false;
@@ -38,24 +38,21 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        if (!blockInput)
-        {
-            Suicide();
-            CheckFirstInput();
-            Move();
-            Jump();
-            TimeSinceJump += Time.deltaTime;
-        }
+        Suicide();
+        CheckFirstInput();
+        Move();
+        Jump();
+        TimeSinceJump += Time.deltaTime;
+        
     }
 
     private void FixedUpdate()
     {
-        if (Input.GetAxis("Horizontal") == 0 && IsGrounded() && rb.velocity.y == 0)
-
+        if(Input.GetAxis("Horizontal")==0 && IsGrounded() && rb.velocity.y==0)
         {
-            rb.velocity = Vector3.zero;
+            rb.velocity= Vector3.zero;
         }
-        ChangePlayerOrientation(); 
+        ChangePlayerOrientation();
     }
 
     private void Jump()
@@ -70,41 +67,34 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move()
     {
-        if (IsGrounded())
-        {
-            horisontalMovemant = playerSpeed * Input.GetAxis("Horizontal");
+        
+            horisontalMovemant=playerSpeed * Input.GetAxis("Horizontal");
             Vector3 tagetVelocity = new Vector2(horisontalMovemant, rb.velocity.y);
-            rb.velocity = Vector3.SmoothDamp(rb.velocity, tagetVelocity, ref velocity, .2f);
-        }
-        else
-        {
-            print("no");
-        }
-
+            rb.velocity = Vector3.SmoothDamp(rb.velocity,tagetVelocity,ref velocity,.2f);
+        
+        
     }
-
     private void Suicide()
     {
         if (Input.GetButtonUp("Fire1") && firstInput)
         {
             FakeDeath();
         }
-
+        
     }
-
     private void CheckFirstInput()
     {
-
+        
         if (!firstInput)
         {
             if (Input.GetButtonUp("Jump") || Input.GetAxis("Horizontal") != 0)
             {
+                Debug.Log("input");
+                Debug.Log($"current = {ghostTrail.currentNbrClones}");
                 firstInput = true;
                 ghostTrail.StartRegister();
-                
                 if (ghostTrail.currentNbrClones != 0)
                 {
-                    ghostTrail.CloneSpawnNbr();
                     ghostTrail.StartSpawning();
                 }
             }
@@ -115,20 +105,39 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    
+    
+    private bool IsGrounded()
+    {
+        return Physics.CheckSphere(groundCheck.position, 0.5f, ground);
+        
+    }
+    private void ChangePlayerOrientation()
+    {
+        float horizontalInput = Input.GetAxis(HorizontalAxis);
+        if (horizontalInput> InputThreshold)
+        {
+            transform.rotation = Quaternion.Euler(RotationZero, RotationZero, RotationZero);
+        }
+        else if (horizontalInput < -InputThreshold)
+        {
+            transform.rotation = Quaternion.Euler(RotationZero,180, RotationZero);
+        }
+    }
+    
     private void FakeDeath()
     {
         blockInput = true;
         ghostTrail.blockWorking= true;
         ghostTrail.canRegister = false;
         Debug.Log($" can register ={ghostTrail.canRegister}");
-        var explo = Instantiate(explosion, transform.position, quaternion.identity);
+        var explo = Instantiate(explosion, transform.position, Quaternion.identity);
         explo.Play();
         rb.velocity = new Vector3(0, 0, 0);
         rb.isKinematic = true;
         var iSpawn = GameManager.Instance.SpawnPlayerTimer();
         GameManager.Instance.StartCoroutine(iSpawn);
     }
-
     public IEnumerator StartMoving()
     {
         ghostTrail.blockWorking = false;
@@ -139,27 +148,7 @@ public class PlayerMovement : MonoBehaviour
         firstInput = false;
         
     }
-
-
-
-    private bool IsGrounded() 
-    { 
-        return Physics.CheckSphere(groundCheck.position, 0.5f, ground); 
-         
-    } 
-    private void ChangePlayerOrientation() 
-    { 
-        float horizontalInput = Input.GetAxis(HorizontalAxis); 
-        if (horizontalInput> InputThreshold) 
-        { 
-            transform.rotation = Quaternion.Euler(RotationZero, RotationZero, RotationZero); 
-        } 
-        else if (horizontalInput < -InputThreshold) 
-        { 
-            transform.rotation = Quaternion.Euler(RotationZero,180, RotationZero); 
-        } 
-    } 
-     
- 
     
-} 
+
+   
+}
--- Assets/Scripts/GhostTrail.cs
+++ Assets/Scripts/GhostTrail.cs
@@ -6,40 +6,25 @@ using Unity.VisualScripting;
 using UnityEngine.Serialization;
 using System.Linq;
 using static GameManager;
-//[ExecuteInEditMode]
-//[]
+
 public class GhostTrail : MonoBehaviour
 {
     public VectorListData vectorListData;
     public float cloneRegisterTime;
     public float cloneSpawnTimer;
-    public  GameObject Clone;
-    /*[HideInInspector]*/ public bool canSpawn= false;
-    /*[HideInInspector]*/public bool canRegister= false;
-    private bool startSpawn;
-    /*[HideInInspector]*/public bool blockWorking=false;
-    private int currentSpawnIndex = 0;
-    private int currentPosSpawn = 0;
-    private int currentPosRegister = 0;
-    private int nonNullList=0;
-    private bool hasSpawnClones=false;
-    //public List<List<GameObject> WeaponSlots = new List<GameObject>(4);
-    public List<Vector2>[] ListParentPos2D;
-   
-    private GameObject[] GO_Clone;
-        // peut faire un dictionnaire
-    // je vais les mettre dans un scriptable object et copier le contenue a chaque mort  ici 
-    public int maxNbrOfClones= 3;
-    [HideInInspector]public int currentNbrClones=0;
+    public GameObject Clone;
+    public bool canSpawn = false;
+    public bool canRegister = false;
+    public bool blockWorking = false;
+    public int maxNbrOfClones = 3;
+    [HideInInspector] public int currentNbrClones = 0;
 
-    private enum GhostState
-    {
-        clone1,
-        clone2,
-        clone3,
-        Dead
-        
-    }
+    private List<Vector2>[] ListParentPos2D;
+    private GameObject[] GO_Clone;
+    private int currentSpawnIndex = 0;
+    private int nonNullList = 0;
+    private bool hasSpawnClones = false;
+
     private void Awake()
     {
         GO_Clone = new GameObject[maxNbrOfClones];
@@ -47,22 +32,7 @@ public class GhostTrail : MonoBehaviour
         for (int i = 0; i < maxNbrOfClones; i++)
         {
             ListParentPos2D[i] = new List<Vector2>();
-            
         }
-        
-        
-    }
-
-    private void Start()
-    {
-        //vectorListData.vectorList.Clear();
-        //CloneSpawnNbr();
-    }
-
-    public void FixedUpdate()
-    {
-        
-        
     }
 
     private void Update()
@@ -73,26 +43,20 @@ public class GhostTrail : MonoBehaviour
             {
                 Debug.Log("ENDSpawn");
                 canSpawn = false;
-                
                 return;
             }
-
             SpawnCloneManager();
         }
-        
     }
 
     private void LateUpdate()
     {
-        Debug.Log($"NOMBRE DE CLONE = {currentNbrClones}");
         if (!canSpawn && canRegister && !blockWorking)
         {
-            
             StartCoroutine(RegisteringClone(cloneRegisterTime));
         }
-
-        
     }
+
     public void StartRegister()
     {
         canRegister = true;
@@ -100,6 +64,7 @@ public class GhostTrail : MonoBehaviour
         canSpawn = false;
         Debug.Log("startREGISTER");
     }
+
     public void StartSpawning()
     {
         canRegister = false;
@@ -107,38 +72,70 @@ public class GhostTrail : MonoBehaviour
         canSpawn = true;
         Debug.Log("startSPAWN");
     }
-    
 
     private IEnumerator RegisteringClone(float TimeSpawn)
     {
         canRegister = false;
         Debug.Log("Registering");
-        
-            ListParentPos2D[currentNbrClones].Add(transform.position);
-            if (ListParentPos2D[currentNbrClones].Count > 0)
-            {
-                Debug.Log($"au dessus = {ListParentPos2D[currentNbrClones]}"); 
-            }
-            else
-            {
-                Debug.Log($"en dessous = {ListParentPos2D[currentNbrClones]}"); 
-            }
 
-            yield return null;
-        
+        ListParentPos2D[currentNbrClones].Add(transform.position);
+
+        if (ListParentPos2D[currentNbrClones].Count > 0)
+        {
+            Debug.Log($"au dessus = {ListParentPos2D[currentNbrClones]}");
+        }
+        else
+        {
+            Debug.Log($"en dessous = {ListParentPos2D[currentNbrClones]}");
+        }
+
+        yield return null;
+
         if (!canSpawn && !blockWorking)
         {
             canRegister = true;
         }
     }
 
-    private IEnumerator MoveClone(List<Vector2> SubPos2D,int currentSpawnIndexIE,float Spawntime,int GO_enemy)
+    private void SpawnCloneManager()
     {
-        GO_Clone[GO_enemy].transform.position =  SubPos2D[currentSpawnIndexIE];
-        yield return null;
-        //yield return new WaitForSeconds(Spawntime);
-        //yield return new WaitForSeconds(cloneSpawnTimer*Time.fixedDeltaTime);
+        if (!hasSpawnClones)
+        {
+            hasSpawnClones = true;
+            for (int i = 0; i < nonNullList; i++)
+            {
+                var PosSpawn = ListParentPos2D[0][0];
+                GO_Clone[i] = Instantiate(Clone, new Vector3(PosSpawn.x, PosSpawn.y, transform.position.z), Quaternion.identity);
+            }
+        }
 
+        int allNullList = 0;
+        for (int i = 0; i < nonNullList; i++)
+        {
+            if (ListParentPos2D[i] != null && currentSpawnIndex < ListParentPos2D[i].Count && ListParentPos2D[i][currentSpawnIndex] != null)
+            {
+                StartCoroutine(MoveClone(ListParentPos2D[i], currentSpawnIndex, cloneSpawnTimer, i));
+            }
+            else
+            {
+                allNullList++;
+            }
+        }
+
+        if (allNullList == nonNullList)
+        {
+            Debug.Log("dqsdsfdsfgdsfdsfdsffdsffd");
+            canSpawn = false;
+            currentSpawnIndex = 0;
+        }
+
+        currentSpawnIndex++;
+    }
+
+    private IEnumerator MoveClone(List<Vector2> SubPos2D, int currentSpawnIndexIE, float Spawntime, int GO_enemy)
+    {
+        GO_Clone[GO_enemy].transform.position = SubPos2D[currentSpawnIndexIE];
+        yield return null;
     }
 
     public void CloneSpawnNbr()
@@ -147,60 +144,17 @@ public class GhostTrail : MonoBehaviour
         nonNullList = 0;
         foreach (var listChildPos in ListParentPos2D)
         {
-            if (listChildPos.Count > 0 )
+            if (listChildPos.Count > 0)
             {
-               
                 nonNullList++;
             }
-           
         }
         Debug.Log($"liste de non null = {nonNullList}");
-        
     }
 
-    private void SpawnCloneManager()
-    {
-        if (!hasSpawnClones)
-        {
-            
-            hasSpawnClones = true;
-            for (int i = 0; i < nonNullList; i++)
-            {
-                Debug.Log($"nombre de clo,e ={currentNbrClones}");
-                var PosSpawn = ListParentPos2D[0][0];
-                GO_Clone[i] = Instantiate(Clone, new Vector3(PosSpawn.x, PosSpawn.y, transform.position.z),
-                    Quaternion.identity);
-            }
-        }
-        int allNullList = 0;
-        
-        for (int i = 0; i < nonNullList; i++)
-        {
-            if (ListParentPos2D[i] != null && currentSpawnIndex < ListParentPos2D[i].Count && ListParentPos2D[i][currentSpawnIndex] != null)
-            {
-                //Debug.Log($"Index out of range: currentSpawnIndex ({currentSpawnIndex}) >= ListParentPos2D[{i}].Count ({ListParentPos2D[i].Count})");
-                StartCoroutine(MoveClone(ListParentPos2D[i], currentSpawnIndex,cloneSpawnTimer,i ));
-            }
-            else
-            {
-                allNullList++;
-            }
-            
-        }
-        if(allNullList == nonNullList)
-        {
-            Debug.Log("dqsdsfdsfgdsfdsfdsffdsffd");
-            //explosion
-            canSpawn = false;
-            currentSpawnIndex = 0;
-        }
-
-        currentSpawnIndex++;
-    }
     public void ChangeCurrentState()
     {
-        
-        if (currentNbrClones-2==maxNbrOfClones)
+        if (currentNbrClones - 2 == maxNbrOfClones)
         {
             GameManager.Instance.UpdateGameState(GameState.Lose);
             Debug.Log("dead");
@@ -208,11 +162,8 @@ public class GhostTrail : MonoBehaviour
         else
         {
             currentSpawnIndex = 0;
-            //currentPosRegister = 0;
-            currentNbrClones+=1;
+            currentNbrClones += 1;
             hasSpawnClones = false;
-
         }
     }
-    
 }
